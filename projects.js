bcb6cc211c633398f21daeee2023f824e421496b
kowabungappl@gmail.com <kowabungappl@gmail.com>
2014-10-18 19:23:32 -0400

    added a project router

--- projects.js
+++ projects.js
@@ -5,28 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var projects = require('./projects');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Load JS from bower
-app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
+// apps
+var therainiscoming = require('../yo-rain/app');
 
-app.use('/', routes);
-app.use('/projects', projects);
+app.use('/therainiscoming', therainiscoming);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
