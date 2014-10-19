ad6944aaef585bded3d8f495cf2f3a9752644600
kowabungappl@gmail.com <kowabungappl@gmail.com>
2014-10-18 21:14:25 -0400

    fix path to apps

--- projects.js
+++ projects.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
 // apps
+// projects/therainiscoming
 var therainiscoming = require('../projects/therainiscoming/app');
 
 app.use('/therainiscoming', therainiscoming);
